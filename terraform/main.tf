0f469a281127bf24842458973bf21b34f1ca2fcc
Larry YU <larry02046@gmail.com>
2025-11-10 01:10:24 +1100

    Deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,29 +6,6 @@ resource "azuread_group" "id_engineers" {
   description       = "Engineering group for identity team (${var.environment})."
 }
 
-# Example 2: An application + service principal...
-resource "azuread_application" "sample_app" {
-  display_name = "sample-app-${var.environment}"
-  owners       = [] # optionally add owners by objectId
-  web {
-    redirect_uris = ["https://localhost/auth/callback"]
-  }
-
-  # Example App Role
-  app_roles = [{
-    allowed_member_types = ["User"]
-    description          = "Read data"
-    display_name         = "Reader"
-    id                   = "00000000-0000-0000-0000-000000000001"
-    is_enabled           = true
-    value                = "Reader"
-  }]
-}
-
-resource "azuread_service_principal" "sample_sp" {
-  application_id = azuread_application.sample_app.application_id
-}
-
 output "group_object_id" {
   value = azuread_group.id_engineers.object_id
 }
