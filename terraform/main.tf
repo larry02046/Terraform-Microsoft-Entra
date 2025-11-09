922c4beccf711466bfad746bdf1c7912078a5d16
Larry YU <larry02046@gmail.com>
2025-11-10 01:32:58 +1100

    Final Deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,14 @@
-# Example 1: A group.
-resource "azuread_group" "id_engineers" {
-  display_name      = "ID Engineers - Dev"
-  mail_enabled      = false
-  security_enabled  = true
-  description       = "Engineering group for identity team."
+provider "azuread" {
+  # With OIDC, provider reads ARM_* env vars set by the workflow.
+  # Optionally pin tenant_id:
+  tenant_id = var.tenant_id
 }
 
+resource "azuread_group" "example" {
+  display_name            = var.group_display_name
+  description             = var.group_description
+  mail_enabled            = false
+  security_enabled        = true
+  prevent_duplicate_names = true
+
+}
