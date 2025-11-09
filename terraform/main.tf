711590f4a7f906db1c40c5089f2f2a6d214a9baa
Larry YU <larry02046@gmail.com>
2025-11-10 01:17:25 +1100

    Deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,8 @@
 # Example 1: A group.
 resource "azuread_group" "id_engineers" {
-  display_name      = "ID Engineers - ${var.environment}"
+  display_name      = "ID Engineers - Dev"
   mail_enabled      = false
   security_enabled  = true
-  description       = "Engineering group for identity team (${var.environment})."
+  description       = "Engineering group for identity team."
 }
 
-output "group_object_id" {
-  value = azuread_group.id_engineers.object_id
-}
-
-output "app_client_id" {
-  value = azuread_application.sample_app.application_id
-}
