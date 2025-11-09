17861e4179e1a814df1a837dc5a7b686bc05f9ba
Larry YU <larry02046@gmail.com>
2025-11-10 01:49:30 +1100

    Final Deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,12 @@
-resource "azuread_group" "example" {
+resource "azuread_group" "tf_example" {
   display_name            = var.group_display_name
   description             = var.group_description
   mail_enabled            = false
   security_enabled        = true
   prevent_duplicate_names = true
 
+}
+
+output "group_object_id" {
+  value = azuread_group.tf_example.object_id
 }
