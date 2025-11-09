489cb65defd8fe9b0532a4d2c6874d7f27731da0
Larry YU <larry02046@gmail.com>
2025-11-09 18:06:38 +1100

    update al tf files

--- variables.tf
+++ variables.tf
@@ -1,11 +1,12 @@
-variable "break_glass_group_object_id" {
-  type        = string
-  description = "Object ID of the Break-Glass group to exclude from CA policy."
+variable "tenant_id"           { type = string }
+variable "group_display_name"  { type = string }
+variable "group_description"   { type = string  default = "Managed by Terraform" }
+variable "group_owner_object_ids" {
+  type    = list(string)
+  default = []
 }
 
-# Optional trusted named location ID (object ID of the Named Location)
-variable "trusted_named_location_id" {
-  type        = string
-  description = "Object ID of a trusted Named Location to exclude."
-  default     = null
-}
+# state backend vars (only if using azurerm backend)
+variable "state_rg_name"   { type = string }
+variable "state_sa_name"   { type = string }
+variable "state_container" { type = string }
